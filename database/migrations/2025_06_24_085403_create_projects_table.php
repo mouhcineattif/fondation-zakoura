7f254cf8959341df4c633a8755951793b0bbeafe
mouhcineattif-in-ctech <amohcine@in-ctech.com>
2025-06-24 10:16:36 +0100

    refactor(migration): clean up comments in projects table migration for clarity

--- database/migrations/2025_06_24_085403_create_projects_table.php
+++ database/migrations/2025_06_24_085403_create_projects_table.php
@@ -11,39 +11,32 @@ class CreateProjectsTable extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
 
-            // Informations générales
-            $table->string('nom_projet'); // Texte court obligatoire
-            $table->string('code_projet'); // Texte court obligatoire
+            $table->string('nom_projet');
+            $table->string('code_projet');
             $table->enum('nature_projet', ['Opérationnel', 'Institutionnel', 'Expérimental'])->nullable(false);
             $table->enum('type_projet', ['Alphabétisation', 'Éducation', 'Formation', 'Sensibilisation'])->nullable(false);
 
-            // Dates du projet
             $table->enum('statut_projet', ['Initiation', 'Conception', 'En cours', 'Terminé', 'Archivé']);
             $table->date('date_lancement');
             $table->date('date_cloture');
             $table->date('date_debut_reelle')->nullable();
 
-            // Responsabilités
             $table->unsignedBigInteger('responsable_id'); // utilisateur SI
             $table->json('partenaires'); // multiple IDs
             $table->enum('role_partenaire', ['Principal', 'Co-porteur', 'Financier', 'Appui technique']);
 
-            // Budget
             $table->decimal('budget_total', 15, 2); // en MAD ou devise liée
             $table->decimal('quote_part_partenaires', 15, 2)->nullable();
             $table->decimal('apport_fz', 15, 2)->nullable();
 
-            // Infos bancaires
             $table->string('banque'); // BMCE, CIH, BOA...
             $table->string('agence_bancaire')->nullable();
             $table->string('rib');
 
-            // Notes / Références / Générés automatiquement
             $table->text('notes')->nullable();
             $table->string('code_automatique')->nullable()->unique();
             $table->timestamps();
 
-            // Foreign key (si users table existe)
             $table->foreign('responsable_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
